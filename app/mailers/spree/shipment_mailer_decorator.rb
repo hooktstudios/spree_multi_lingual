271812cc8f7a99232f46fa8cc39ee5e0a4df4c62
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2014-05-12 16:17:49 -0400

    Fix custom mailer method argument usage

--- app/mailers/spree/shipment_mailer_decorator.rb
+++ app/mailers/spree/shipment_mailer_decorator.rb
@@ -2,7 +2,7 @@ Spree::ShipmentMailer.class_eval do
   helper 'spree/base'
 
   def shipped_email(shipment, resend=false)
-    @shipment = shipment
+    @shipment = shipment.respond_to?(:id) ? shipment : Spree::Shipment.find(shipment)
     I18n.locale = @shipment.order.locale
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : "")
     subject += "#{Spree::Config[:site_name]} #{Spree.t('shipment_mailer.shipped_email.subject')} ##{@shipment.order.number}"
